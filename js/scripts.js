d56b010c13f0ecae7a7456b9f32dd1bf74c56fc8
CommanderBlak <mwandiwayne@gmail.com>
2017-06-07 10:49:15 +0300

    adds initial directories and files

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,20 @@
+var leapYear = function(input) {
+  if (input % 4 === 0) && (100 !== 0){
+    return true;
+  }
+  else {
+    return false;
+  }
+
+};
+
+
 $(document).ready(function() {
-  
+  $(".form-leap").submit(function(event) {
+    event.preventDefault();
+    var input = $("#leapyear").val();
+    var result = leapYear(input);
+
+    $(".results").text(result);
+  });
 });
