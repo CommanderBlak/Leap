8bfdc34ff8b8e5f4f0eff5cd88fbc6e78e512015
CommanderBlak <mwandiwayne@gmail.com>
2017-06-07 13:01:27 +0300

    adds final conditions in js and the README file

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,28 @@
+// business logic
 var leapYear = function(input) {
-  if ((input % 4 === 0) && (input % 100 !== 0)) {
+  if ((input % 4 === 0) && (input % 100 !== 0) || (input % 400 === 0)) {
     return true;
-  }
-  else {
+  } else {
     return false;
   }
 
 };
 
 
+// user interface logic
 $(document).ready(function() {
   $(".form-leap").submit(function(event) {
     event.preventDefault();
-    var input = $("#leapyear").val();
+    var input = parseInt($("#leapyear").val());
     var result = leapYear(input);
 
-    alert(result);
+    $(".year").text(input);
+
+    if (!result) { // result === false
+      $(".not").text("not");
+    } else {
+      $(".not").text("");
+    }
+    $("#results").show();
   });
 });
